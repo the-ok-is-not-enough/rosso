7d9ad6c501042f430d4441f3b1a1bd2bc41cad8e
Steven Penny <rqwwof@riseup.net>
2023-03-25 23:29:01 -0500

    deleted:    blog/2023-03/decode.go

--- blog/2023-03/printable.go
+++ blog/2023-03/printable.go
@@ -1,8 +1,14 @@
 package printable
 
-import "unicode/utf8"
+import (
+   "encoding/hex"
+   "errors"
+   "unicode/utf8"
+)
 
 const escape_character = '~'
+
+var error_escape = errors.New("invalid printable escape")
 
 // mimesniff.spec.whatwg.org#binary-data-byte
 func binary_byte(b byte) bool {
@@ -21,6 +27,28 @@ func binary_byte(b byte) bool {
    return false
 }
 
+func decode(src string) ([]byte, error) {
+   var dst []byte
+   for len(src) >= 1 {
+      s := src[0]
+      if s == escape_character {
+         if len(src) <= 2 {
+            return nil, error_escape
+         }
+         d, err := hex.DecodeString(src[1:3])
+         if err != nil {
+            return nil, err
+         }
+         dst = append(dst, d...)
+         src = src[3:]
+      } else {
+         dst = append(dst, s)
+         src = src[1:]
+      }
+   }
+   return dst, nil
+}
+
 func decode_rune(p []byte) (rune, int) {
    if len(p) >= 1 {
       b := p[0]
@@ -29,4 +57,22 @@ func decode_rune(p []byte) (rune, int) {
       }
    }
    return utf8.DecodeRune(p)
+}
+
+func encode(src []byte) string {
+   var dst []byte
+   for len(src) >= 1 {
+      r, size := decode_rune(src)
+      s := src[:size]
+      if r == utf8.RuneError && size == 1 {
+         var d [2]byte
+         hex.Encode(d[:], s)
+         dst = append(dst, escape_character)
+         dst = append(dst, d[:]...)
+      } else {
+         dst = append(dst, s...)
+      }
+      src = src[size:]
+   }
+   return string(dst)
 }
--- blog/2023-03/printable_test.go
+++ blog/2023-03/printable_test.go
@@ -1,43 +1,52 @@
 package printable
 
 import (
-   "fmt"
+   "bytes"
    "net/http"
    "net/http/httputil"
    "testing"
 )
+
+func Test_Binary(t *testing.T) {
+   src, dst, err := round_trip(binary)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if !bytes.Equal(src, dst) {
+      t.Fatal(dst)
+   }
+}
+
+func round_trip(s string) ([]byte, []byte, error) {
+   res, err := http.Get(s)
+   if err != nil {
+      return nil, nil, err
+   }
+   src, err := httputil.DumpResponse(res, true)
+   if err != nil {
+      return nil, nil, err
+   }
+   if err := res.Body.Close(); err != nil {
+      return nil, nil, err
+   }
+   dst, err := decode(encode(src))
+   if err != nil {
+      return nil, nil, err
+   }
+   return src, dst, nil
+}
 
 const (
    binary = "https://picsum.photos/1"
    text = "http://httpbin.org/get"
 )
 
-func Test_Binary(t *testing.T) {
-   res, err := http.Get(binary)
-   if err != nil {
-      t.Fatal(err)
-   }
-   dump, err := httputil.DumpResponse(res, true)
-   if err != nil {
-      t.Fatal(err)
-   }
-   if err := res.Body.Close(); err != nil {
-      t.Fatal(err)
-   }
-   fmt.Println(string(encode(dump)))
-}
-
 func Test_Text(t *testing.T) {
-   res, err := http.Get(text)
+   src, dst, err := round_trip(text)
    if err != nil {
       t.Fatal(err)
    }
-   dump, err := httputil.DumpResponse(res, true)
-   if err != nil {
-      t.Fatal(err)
+   if !bytes.Equal(src, dst) {
+      t.Fatal(dst)
    }
-   if err := res.Body.Close(); err != nil {
-      t.Fatal(err)
-   }
-   fmt.Println(string(encode(dump)))
 }
